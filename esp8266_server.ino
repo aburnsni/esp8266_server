aadeceb99d7c9e5fd31fee4fc2302667b4a60a90
Andrew Burns <aburnsni@gmail.com>
2017-10-03 11:15:18 +0100

    Added option for 2 different SSIDs - still need to set array for multiple SSIDs

--- esp8266_server.ino
+++ esp8266_server.ino
@@ -8,31 +8,40 @@
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 ESP8266WebServer server(80);
 
+unsigned long startMillis = 0;
+unsigned long wifitimout = 20000;
+
 void setup() {
+  Serial.begin(115200);
   lcd.init();   // initializing the LCD
   lcd.backlight(); // Enable or Turn On the backlight
-  delay(200);
 
   // We start by connecting to a WiFi network
   lcd.setCursor(0, 0);
   lcd.print("Connecting");
-
+  Serial.println("connecting");
   WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
+  lcd.setCursor(0, 1);
+  lcd.print("H: ");
+  lcd.print(ssidh);
+  for (int i = strlen(ssidh)+3; i < 16; i=i+1) {
+    Serial.print(strlen(ssidh));
+    lcd.print(" ");
+  }
+  // lcd.print("             ");
+  Serial.println("Home");
+  wifiConnect(ssidh, passwordh);
 
-  int i = 4;
-  while (WiFi.status() != WL_CONNECTED) { // Wait for WIFI to connect
-    delay(500);
-    Serial.print(".");
-    if (i < 4) {
-      lcd.setCursor(10 + i, 0);
-      lcd.print (".");
-      i++;
-    } else {
-      lcd.setCursor(10, 0);
-      lcd.print ("    ");
-      i = 0;
+  if (WiFi.status() != WL_CONNECTED) {
+    lcd.setCursor(0, 1); 
+    lcd.print("W: "); 
+    lcd.print(ssidw);
+    for (int i = strlen(ssidw)+3; i < 16; i=i+1) {
+      Serial.print(strlen(ssidw));
+      lcd.print(" ");
     }
+    Serial.println("Work");
+    wifiConnect(ssidw, passwordw);
   }
 
   lcd.setCursor(0, 0);
@@ -58,6 +67,26 @@ void setup() {
 
 void loop() {
   server.handleClient();
+}
+
+void wifiConnect(const char* ssid, const char* password) {
+  startMillis = millis();
+  Serial.print("SSID: ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  int i = 4;
+  while ((WiFi.status() != WL_CONNECTED) && (millis() - startMillis < wifitimout)) { // Wait for WIFI to connect
+    delay(500);
+    if (i < 4) {
+      lcd.setCursor(10 + i, 0);
+      lcd.print (".");
+      i++;
+    } else {
+      lcd.setCursor(10, 0);
+      lcd.print ("    ");
+      i = 0;
+    }
+  }
 }
 
 void setupPageHandlers() {
